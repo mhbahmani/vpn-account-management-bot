2008530f4588a2ca722157995684cfb188ed1992
mhbahmani <mhbahmani79@gmail.com>
2021-02-07 22:06:35 +0330

    setup database

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from decouple import config
 import botogram
+import pymongo
+from pymongo import MongoClient
 
 
 bot = botogram.create(config("API_KEY"))
 bot.about = config("about")
 bot.owner = config("owner")
 
+client = MongoClient()
+db = client.vpnbotdb
+users = db.users
 
 if __name__ == "__main__":
     bot.run()
